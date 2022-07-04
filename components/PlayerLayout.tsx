0a7a5eafa3a198537c6aca8e0bcfd6c27ebb9339
Mario Portillo <mportilo80@gmail.com>
2022-07-04 14:01:49 -0500

    flesh out layout components

--- components/PlayerLayout.tsx
+++ components/PlayerLayout.tsx
@@ -1,15 +1,30 @@
 import { Box } from "@chakra-ui/layout";
 
+import Sidebar from "./Sidebar";
+
 const PlayerLayout = ({ children }) => {
   return (
     <Box width="100vh" height="100vh">
       <Box position="absolute" top="0" width="250px" left="0">
-        Sidebar
+        <Sidebar />
       </Box>
-      <Box marginLeft="250px" marginBottom="100px">
+      <Box
+        marginLeft="250px"
+        marginBottom="100px"
+        backgroundColor="gold"
+        width="calc(100vw - 250px)"
+        height="calc(100vh - 100px)"
+      >
         {children}
       </Box>
-      <Box position="absolute" left="0" bottom="0">
+      <Box
+        position="absolute"
+        left="0"
+        bottom="0"
+        height="100px"
+        width="100%"
+        backgroundColor="blue"
+      >
         Player
       </Box>
     </Box>
